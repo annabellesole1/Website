294b41e7bb3e14e3ac47d873669628caaefb441b
annabellesole1 <annabellesole1@gmail.com>
2024-03-20 21:54:30 -0700

    fixed form bug again

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,6 +31,7 @@ export default function Contact() {
         <form
           netlify
           name="contact"
+          onSubmit={handleSubmit}
           className="w-full flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
