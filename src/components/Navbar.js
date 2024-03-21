7715506a7bbd1f8a3b026806bbbce24e52657f8d
annabellesole1 <annabellesole1@gmail.com>
2024-03-20 22:01:03 -0700

    Fixed href bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <header className="bg-white md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-black mb-4 md:mb-0">
+        <a href = "#" className="title-font font-medium text-black mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl text-black">
             Annabelle Sole
           </a>
