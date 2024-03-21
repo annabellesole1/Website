5320bbc3a0905a795a0aefdbdf533fae5a5b1ee5
annabellesole1 <annabellesole1@gmail.com>
2024-03-20 22:09:01 -0700

    Fixed href bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ export default function Navbar() {
   return (
     <header className="bg-white md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href = "#" className="title-font font-medium text-black mb-4 md:mb-0">
+        <button className="title-font font-medium text-black mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl text-black">
             Annabelle Sole
           </a>
-        </a>
+        </button>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-black justify-center">
           <a href="#projects" className="mr-5 hover:text-gray-400">
             Past Work
